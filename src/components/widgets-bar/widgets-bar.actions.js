d1cf083a136535375618fd312aed11da1ff88640
Sharath Gudimella <Sharath.Gudimella@uppal.ggktech.local>
2018-04-02 19:37:19 +0530

    name changes

--- src/components/widgets-bar/widgets-bar.actions.js
+++ src/components/widgets-bar/widgets-bar.actions.js
@@ -2,7 +2,7 @@ import * as  widgetBarConstants from "./widgets-bar.constants";
 import _ from 'lodash';
 
 
-export function updatePropertyAction(key, value) {
+export function updateProperty(key, value) {
     return (dispatch, getState) => {
         dispatch({
             type: widgetBarConstants.UPDATE_DASHBOARD_PROPERTY,
@@ -12,7 +12,7 @@ export function updatePropertyAction(key, value) {
     }
 }
 
-export function saveDashboardAction() {
+export function saveDashboard() {
     return (dispatch, getState) => {
        // let dashboardData = MapDashboard(getState().widgetsBar, getState().dashboard);
         // dispatch({
