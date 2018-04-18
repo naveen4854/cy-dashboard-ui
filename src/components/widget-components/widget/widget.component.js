e4a682c9a292957997228ca9b06ef4385b4144b5
Sharath Gudimella <Sharath.Gudimella@uppal.ggktech.local>
2018-04-18 20:27:38 +0530

    restricting unnecessary calls in edit mode

--- src/components/widget-components/widget/widget.component.js
+++ src/components/widget-components/widget/widget.component.js
@@ -22,13 +22,17 @@ export default class WidgetComponent extends PureComponent {
     constructor(props) {
         super(props);
         // debugger
-        console.log('constructor ', this.props.widget.refreshInterval)
-        this.props.pullWidgetData(this.props.dashboardId, this.props.widget.id, this.props.widget.refreshInterval)
+        if (this.props.dashboardMode == DashboardModeEnum.View || this.props.dashboardMode == DashboardModeEnum.Slider) {
+            console.log('constructor ', this.props.widget.refreshInterval)
+            this.props.pullWidgetData(this.props.dashboardId, this.props.widget.id, this.props.widget.refreshInterval)
+        }
     }
     componentWillReceiveProps(nextProps) {
-        console.log('receive props')
-        if(this.props.widget.id != nextProps.widget.id)
-        this.props.pullWidgetData(nextProps.dashboardId, nextProps.widget.id,  nextProps.widget.refreshInterval)
+        if (this.props.dashboardMode == DashboardModeEnum.View || this.props.dashboardMode == DashboardModeEnum.Slider) {
+            console.log('receive props')
+            if (this.props.widget.id != nextProps.widget.id)
+                this.props.pullWidgetData(nextProps.dashboardId, nextProps.widget.id, nextProps.widget.refreshInterval)
+        }
     }
     render() {
         let classToBeApplied = 'widget';
