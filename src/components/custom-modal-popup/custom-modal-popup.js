0c4a1fd801cfe04ee970541d2221a52a184cbecb
shilpatippana <37687025+shilpatippana@users.noreply.github.com>
2018-04-23 18:34:18 +0530

    Enter to save the dshboard in the popup and mimimizing the popup

--- src/components/custom-modal-popup/custom-modal-popup.js
+++ src/components/custom-modal-popup/custom-modal-popup.js
@@ -1,61 +1,72 @@
 import React from 'react';
-import { Modal, Effect  } from 'react-dynamic-modal';
+import { Modal, Effect } from 'react-dynamic-modal';
 import _ from 'lodash';
 
 export class CustomModalPopUp extends React.Component {
     constructor(props) {
         super(props);
-        this.state=props;
+        this.state = props;
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     updateProp(e) {
-        
+
         this.setState({ name: e.target.value });
     }
-
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.SaveClick(this.state.name);
+    }
 
     render() {
         const { onRequestClose, updateName, OnCancel } = this.props;
+        let contentStyles =
+            {
+                content: {
+                    width: '40%',
+                }
+            }
         return (
 
-            <Modal
+            <Modal style={contentStyles}
                 onRequestClose={onRequestClose}
                 effect={Effect.ScaleUp}
             >
+                <form onSubmit={this.handleSubmit}>
+                    <div className="panel panel-default">
+                        <div className="panel-heading"> <strong className="">{this.props.title}</strong>
 
-                <div className="panel panel-default">
-                    <div className="panel-heading"> <strong className="">{this.props.title}</strong>
-
-                    </div>
-                    <div className="panel-body">
-                        <div className="row" >
-                            <div className="form-group">
-                                <label className="col-md-offset-2 col-sm-offset-2 col-md-2 col-sm-3 control-label">{this.props.l.t('Name', 'Name')}</label>
-                                <div className="col-md-5 col-sm-9">
-                                    <input type="text" className="form-control" ref="refDashboardName" placeholder="Dashboard name"
-                                    value={this.state.name}  onChange={(e) => this.updateProp(e)}
-                                    />
+                        </div>
+                        <div className="panel-body">
+                            <div className="row" >
+                                <div className="form-group">
+                                    <label className="col-md-offset-2 col-sm-offset-2 col-md-2 col-sm-3 control-label">{this.props.l.t('Name', 'Name')}</label>
+                                    <div className="col-md-5 col-sm-9">
+                                        <input type="text" className="form-control" ref="refDashboardName" placeholder="Dashboard name"
+                                            value={this.state.name} onChange={(e) => this.updateProp(e)}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row" >
+                                <div className="form-group last modal-popup-margin">
+                                    <div className="col-sm-offset-4 col-sm-8">
+                                        <button type="button" className="btn btn-primary btn-sm" disabled={!_.trim(this.state.name)}
+                                            onClick={() => this.props.SaveClick(this.state.name)}
+                                        >
+                                            {this.props.okButtonText}
+                                        </button>
+                                        <button type="button" className="btn btn-default btn-sm margin10px"
+                                            onClick={OnCancel}>{this.props.l.t('Cancel', 'Cancel')}</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="row" >
-                            <div className="form-group last modal-popup-margin">
-                                <div className="col-sm-offset-4 col-sm-8">
-                                    <button type="button" className="btn btn-primary btn-sm"  disabled={!_.trim(this.state.name)}
-                                        onClick={() => this.props.SaveClick(this.state.name)}
-                                    >
-                                        {this.props.okButtonText}
-                                    </button>
-                                    <button type="button" className="btn btn-default btn-sm margin10px"
-                                        onClick={OnCancel}>{this.props.l.t('Cancel', 'Cancel')}</button>
-                                </div>
-                            </div>
-                        </div>
                     </div>
-                </div>
+                </form>
             </Modal>
 
         );
     }
 }
-export default  CustomModalPopUp
+export default CustomModalPopUp
