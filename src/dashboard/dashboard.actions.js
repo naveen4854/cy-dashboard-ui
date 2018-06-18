823adb26370031073989e157e2761e914377800b
Sharath Gudimella <Sharath.Gudimella@uppal.ggktech.local>
2018-06-18 21:22:41 +0530

    fix for thresholds multiple emails

--- src/dashboard/dashboard.actions.js
+++ src/dashboard/dashboard.actions.js
@@ -239,8 +239,7 @@ export function pullWidget(dashboardId, widgetId, refreshInterval) {
             return;
 
         let setTimeoutId = setTimeout(() => {
-            dashboardService.viewWidgetData(dashboardId, widgetId, {}).then(response => {
-
+            dashboardService.viewWidgetData(dashboardId, widgetId, widget.previousData).then(response => {
                 if (widget) {
                     dispatch(getState().notificationStore.clearNotifications());
                     let updatedWidget = DashboardUtilities.WidgetDataMapper(widget, response.data)
